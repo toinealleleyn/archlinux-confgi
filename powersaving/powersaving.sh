98d37db1ea490c7888eeed3b1f8f8269aa509c78
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2021-07-24 08:28:56 +0200

    Removed redundant powertop service (tlp is enough).

--- powersaving/powersaving.sh
+++ powersaving/powersaving.sh
@@ -6,13 +6,8 @@
 # Install packages.
 sudo pacman -S --needed --noconfirm tlp tlp-rdw powertop
 
-# Create systemd service for powertop (auto-tune).
-sudo cp powertop.service /etc/systemd/system/powertop.service
-
 # Configure the services.
 sudo systemctl enable tlp.service
 sudo systemctl enable NetworkManager-dispatcher.service
 sudo systemctl mask systemd-rfkill.service
 sudo systemctl mask systemd-rfkill.socket
-sudo systemctl enable powertop.service
-
