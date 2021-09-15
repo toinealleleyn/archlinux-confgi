ef69dc2d13be8ca78f347fd1fb7033ad577dbbca
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2021-09-15 15:09:14 +0200

    added vim-plug to neovim

--- neovim/neovim.sh
+++ neovim/neovim.sh
@@ -4,18 +4,34 @@
 sudo pacman -S --noconfirm --needed neovim
 
 # Create config directory
-mkdir -p ${HOME}/.config/nvim
+mkdir -p $HOME/.config/nvim
+
+# Create files
+touch $HOME/.config/nvim/init.vim
+mkdir $HOME/.config/nvim/vim-plug
+touch $HOME/.config/nvim/vim-plug/plugins.vim
+
+# Install and configure vim-plug
+tee $HOME/.config/nvim/vim-plug/plugins.vim << EOF
+" auto-install vim-plug
+if empty(glob('~/.config/nvim/autoload/plug.vim'))
+  silent !curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  "autocmd VimEnter * PlugInstall
+  "autocmd VimEnter * PlugInstall | source $MYVIMRC
+endif
+
+call plug#begin('~/.config/nvim/autoload/plugged')
+
+    " Better Syntax Support
+    Plug 'sheerun/vim-polyglot'
+
+    " Auto pairs for '(' '[' '{'
+    Plug 'jiangmiao/auto-pairs'
+
+call plug#end()
+EOF
 
 # Create init.vim file
-tee ${HOME}/.config/nvim/init.vim << EOF
-set relativenumber
-hi LineNr ctermfg=8
-
-inoremap " ""<left>
-inoremap ' ''<left>
-inoremap ( ()<left>
-inoremap [ []<left>
-inoremap { {}<left>
-inoremap {<CR> {<CR>}<ESC>O
-inoremap {;<CR> {<CR>};<ESC>O
-EOF
+echo "source \$HOME/.config/nvim/vim-plug/plugins.vim" > $HOME/.config/nvim/init.vim
+echo "source \$HOME/.config/nvim/relativenumbers.vim" >> $HOME/.config/nvim/init.vim
