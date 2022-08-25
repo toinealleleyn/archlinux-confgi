9cd87dc3e69999d1f3690f2e3206cec49b5bc5e1
Toine Alleleyn <toine.alleleyn+bitwarden@protonmail.com>
2022-08-25 16:32:07 +0200

    Fixes.

--- gnome/gnome.sh
+++ gnome/gnome.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 # Install extra packages.
-sudo pacman -S --needed --noconfirm iio-sensor-proxy qgnomeplatform
+sudo pacman -S --needed --noconfirm iio-sensor-proxy qgnomeplatform-qt{5,6}
 
 # Libadwaita theme for gtk3 apps.
 if command -v paru
 then
-	paru -S adw-gtk3
+  paru -S adw-gtk3
   gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
 fi
 
 # Set keyboard layout.
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+intl')]"
+
+# Allow wayland with Nvidia in GDM.
+sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rules
