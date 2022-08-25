335914c22b0295c0c1e9367b2c0cfab08d636680
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2022-08-25 15:29:26 +0200

    Added new scripts, removed deprecated ones, and a few updates.

--- systemd-boot/systemd-boot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Script to configure systemd-boot.
-
-# Create pacman hook to update systemd-boot after the systemd package upgrades.
-sudo mkdir -p /etc/pacman.d/hooks/
-sudo cp pacman.hook /etc/pacman.d/hooks/100-systemd-boot.hook
