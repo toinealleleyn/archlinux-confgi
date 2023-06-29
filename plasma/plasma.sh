400f1bf083065a070fcda9de93fa1f89a4a69e28
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2023-06-29 09:36:18 +0200

    Add emoji and cjk fonts.

--- plasma/plasma.sh
+++ plasma/plasma.sh
@@ -5,3 +5,6 @@ sudo pacman -S --needed --noconfirm kdegraphics-thumbnailers \
 	qt5-imageformats \
 	ffmpegthumbs \
 	taglib
+
+# Add emoji and cjk fonts.
+sudo pacman -S --needed --noconfirm noto-fonts-{emoji,cjk}
