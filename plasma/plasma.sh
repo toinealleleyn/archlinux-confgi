a2b8a9f334e45ed42a3a0044ca28188a4cc3f74b
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2023-07-05 08:43:00 +0200

    Additional packages for basic functionalities.

--- plasma/plasma.sh
+++ plasma/plasma.sh
@@ -8,3 +8,11 @@ sudo pacman -S --needed --noconfirm kdegraphics-thumbnailers \
 
 # Add emoji and cjk fonts.
 sudo pacman -S --needed --noconfirm noto-fonts-{emoji,cjk}
+
+# Install xdg-desktop-portal for screensharing in wayland.
+sudo pacman -S --needed --noconfirm xdg-desktop-portal
+
+# Install additional tools for basic functionality.
+sudo pacman -S --needed --noconfirm spectacle \
+	gwenview \
+	okular 
