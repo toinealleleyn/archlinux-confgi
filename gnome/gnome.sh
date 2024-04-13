41d68ca776ce3d2c53400427b5f6eb66bb928135
Toine Alleleyn <toine.alleleyn+github@protonmail.com>
2024-04-13 21:10:14 +0200

    Added configuration for Gnome - 24h clock, keyboard layout, flatpak and adw-gtk3

---
+++ gnome/gnome.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+# Set clock to 24h format
+gsettings set org.gnome.desktop.interface clock-format 24h
+
+# Set keyboard layout
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+intl')]"
+
+# Install flatpak
+sudo pacman -S --needed --noconfirm flatpak
+
+# Enable bluetooth and change default behaviour to "off"
+sudo sed -i 's/^#AutoEnable=true/AutoEnable=false/g' /etc/bluetooth/main.conf
+sudo systemctl enable bluetooth --now
+
+# Set dark mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# adw-gtk3
+mkdir -p ~/.local/share/themes
+curl -sLo "adw-gtk3.tar.xz"  $(curl -s https://api.github.com/repos/lassekongo83/adw-gtk3/releases/latest | grep browser_download_url | cut -d\" -f4)
+tar -xf adw-gtk3.tar.xz -C ~/.local/share/themes/
+rm -rf adw-gtk3.tar.xz
+gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
